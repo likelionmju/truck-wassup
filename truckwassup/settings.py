d57dffd92eb3c981686ba73da2f0f3bca5ea2f42
geonh22 <geonh22@naver.com>
2019-08-09 22:27:59 +0900

    modify signup

--- truckwassup/settings.py
+++ truckwassup/settings.py
@@ -123,6 +123,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'home','static'),
+    os.path.join(BASE_DIR, 'account','static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
