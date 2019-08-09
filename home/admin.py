c9f60f61bfcdb378ad2ec283dc193e3510f835d2
JeongHun <bjh5230@gmail.com>
2019-08-09 21:07:48 +0900

    Add FoodTruck model

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import FoodTruck, Menu
 
 # Register your models here.
+admin.site.register(FoodTruck)
+admin.site.register(Menu)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class FoodTruck(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images/')
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+class Menu(models.Model):
+    foodtruck = models.OneToOneField(FoodTruck, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
