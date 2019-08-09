fd93d9d8d25fe28e1c08e8705e564a848d19a936
JeongHun <bjh5230@gmail.com>
2019-08-09 23:00:49 +0900

    Modified Account_Profile model

--- account/views.py
+++ account/views.py
@@ -3,12 +3,15 @@ from django.contrib.auth.models import User
 from django.contrib import auth
 # Create your views here.
 
+def register(request):
+    return render(request, 'register.html')
 
 def signup(request):
     if request.method == 'POST':
         if request.POST['password1'] == request.POST['password2']:
             user = User.objects.create_user(
                 request.POST['username'], password=request.POST['password1'])
+            
             auth.login(request, user)
             return redirect('home')
     return render(request, 'accounts/signup.html')
