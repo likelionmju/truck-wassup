257c1d4d144310945b3ed049910cc91ec4230c17
Jeongiseok <p99791780@gmail.com>
2019-08-09 19:38:12 +0900

    sign

--- truckwassup/urls.py
+++ truckwassup/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 import home.views
+import account.views
 
 urlpatterns = [
     path('', home.views.home, name = 'home'),
