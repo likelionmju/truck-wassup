aae9eeecceed7cec37be5537d76ef62e90502194
Jeongiseok <p99791780@gmail.com>
2019-08-09 18:28:53 +0900

    home_plz

--- truckwassup/settings.py
+++ truckwassup/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'truckwassup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['truckwassup/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
