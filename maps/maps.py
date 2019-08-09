ea3a05fc275687f83fa7a2dcaee958453bc5e3e0
bbanderson <tombyun@naver.com>
2019-08-10 03:00:25 +0900

    Added Map Page

--- maps/maps.py
+++ maps/maps.py
@@ -7,7 +7,20 @@ import folium
 def maps():
     map1 = folium.Map(location = [37.580421, 126.923437], zoom_start = 20)
     folium.Marker([37.580421, 126.923437], popup = 'Myongji University').add_to(map1)
-    folium.Marker([37.581375, 126.924334], popup = '띵동와플').add_to(map1)
-    map1.save('map.html')
+    folium.Marker([37.581515, 126.925015], popup = '띵동와플').add_to(map1)
+    folium.Marker([37.580057, 126.924613], popup = '핫도그팩토리').add_to(map1)
+    folium.Marker([37.579985, 126.925115], popup = '붕어빵').add_to(map1)
+    folium.Marker([37.573945, 126.923334], popup = '타코야키').add_to(map1)
+    folium.Marker([37.581752, 126.924334], popup = '청년반점').add_to(map1)
+    folium.Marker([37.581375, 126.925334], popup = '키다리푸드트럭').add_to(map1)
+    folium.Marker([37.581215, 126.921334], popup = '기도리').add_to(map1)
+    folium.Marker([37.581175, 126.923434], popup = '청년창업').add_to(map1)
+    folium.Marker([37.581475, 126.926234], popup = '꼬닐스').add_to(map1)
+    map1.save('maps/templates/map.html')
+    return map1
+    
+    
+
+
 
 
--- maps/views.py
+++ maps/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from . import maps
 import folium
+import os
 
 # Create your views here.
 def home(request):
-    return render(request, 'maps.html')
+    mark = maps.maps().get_root().render()
+    #mark.split()
+    #open("./templates/map.html", 'r')
+    #open(os.path.join(STATIC_ROOT,'maps/templates/map.html'), 'r')
+    return render(request, 'maps.html', {'mark':mark})
